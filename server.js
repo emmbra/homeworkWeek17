a7397ac749492e949f43172615e8e40f81f35bd3
Emmett Brady <emmett.brady@gmail.com>
2020-05-18 08:22:27 -0700

    adjusted server settings

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const db = require('./models');
 const logger = require('morgan');
 
 const PORT = process.env.PORT || 3001;
@@ -12,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workoutDB', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout_DB', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false });
 
 require("./routes/apiRoutes")(app);
 require("./routes/viewRoutes")(app);
